7e19dc2e5713c30f9f2521c84041c0c0aa194661
anharsaja <anharmuk12@gmail.com>
2024-12-13 00:29:09 +0700

    feat: create seeder

--- database/migrations/2024_12_12_164341_create_diseases_table.php
+++ database/migrations/2024_12_12_164341_create_diseases_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('diseases', function (Blueprint $table) {
             $table->id();
-            $table->string('code', 50)->unique();
-            $table->string('name', 255); 
+            $table->string('code')->unique();
+            $table->string('name'); 
             $table->timestamps();
         });
     }
