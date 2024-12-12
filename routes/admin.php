a5b65f74cb9dd539846246b1a321465add631824
Heri Setyawan <herisetyawan233@gmail.com>
2024-12-13 00:48:16 +0700

    feat: route admin

---
+++ routes/admin.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::group(["prefix"=> "admin"], function () {
+  Route::get('/', function () {
+    return view('welcome');
+  })->name('dashboard');
+  Route::get('/symptom', function () {
+    return view('welcome');
+  })->name('symptom');
+  Route::get('/disease', function () {
+    return view('welcome');
+  })->name('disease');
+
+});
