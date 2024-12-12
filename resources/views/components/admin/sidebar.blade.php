0eb7cd87f13737cf5f6b26fad2d0b88eeaa3de04
Heri Setyawan <herisetyawan233@gmail.com>
2024-12-13 00:35:41 +0700

    refactor: partialization component

---
+++ resources/views/components/admin/sidebar.blade.php
@@ -0,0 +1,47 @@
+<section class="sidebar">
+    <!-- Sidebar user panel -->
+    <div class="user-panel">
+        <div class="pull-left image">
+            <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+            <p>Alexander Pierce</p>
+        </div>
+    </div>
+    <!-- /.search form -->
+    <!-- sidebar menu: : style can be found in sidebar.less -->
+    <ul class="sidebar-menu">
+        <li class="header">DASHBOARD PASIEN</li>
+        <li class="active">
+            <a href="pages/widgets.html">
+                <i class="fa fa-th"></i> <span>Diagnosa</span>
+            </a>
+        </li>
+        <li class="">
+            <a href="pages/widgets.html">
+                <i class="fa fa-th"></i> <span>Hasil</span>
+            </a>
+        </li>
+        <li class="header">DASHBOARD ADMIN</li>
+        <li class="">
+            <a href="pages/widgets.html">
+                <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+            </a>
+        </li>
+        <li class="">
+            <a href="pages/widgets.html">
+                <i class="fa fa-th"></i> <span>Data Gejala</span>
+            </a>
+        </li>
+        <li class="">
+            <a href="pages/widgets.html">
+                <i class="fa fa-th"></i> <span>Data Penyakit</span>
+            </a>
+        </li>
+        <li class="">
+            <a href="pages/widgets.html">
+                <i class="fa fa-th"></i> <span>Keluar</span>
+            </a>
+        </li>
+    </ul>
+</section>
