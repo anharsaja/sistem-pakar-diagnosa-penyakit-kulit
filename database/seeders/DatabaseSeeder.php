e6d6f5e5029439d6f5a34c2af5bec967edea3e19
anharsaja <anharmuk12@gmail.com>
2024-12-13 00:35:40 +0700

    feat: create seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             DiseaseSeeder::class,
-            SymptomSeeder::class
+            SymptomSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
