fc35c4cf98d8102526e5e782cab6f9e21e67c98b
anharsaja <anharmuk12@gmail.com>
2024-12-12 23:57:27 +0700

    migration

--- database/migrations/2024_12_12_164341_create_diseases_table.php
+++ database/migrations/2024_12_12_164341_create_diseases_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('diseases', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['admin', 'patient'])->default('patient');
-            $table->rememberToken();
+            $table->string('code', 50)->unique();
+            $table->string('name', 255); 
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('diseases');
     }
 };
--- database/migrations/2024_12_12_164405_create_symptoms_table.php
+++ database/migrations/2024_12_12_164405_create_symptoms_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('symptoms', function (Blueprint $table) {
             $table->id();
+            $table->string('code')->unique();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['admin', 'patient'])->default('patient');
-            $table->rememberToken();
+            $table->float('expert_value'); 
+
+            $table->unsignedBigInteger('disease_id');
+            $table->foreign('disease_id')->references('id')->on('diseases')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('symptoms');
     }
 };
