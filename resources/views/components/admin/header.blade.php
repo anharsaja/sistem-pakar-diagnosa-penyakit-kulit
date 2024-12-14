73e8d20723f24968815df90646911aa7cc5d76da
Heri Setyawan <herisetyawan233@gmail.com>
2024-12-14 07:14:13 +0700

    fix: header profile logout

--- resources/views/components/admin/header.blade.php
+++ resources/views/components/admin/header.blade.php
@@ -32,7 +32,13 @@
                         <!-- Menu Footer-->
                         <li class="user-footer">
                             <div class="pull-right">
-                                <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                <a href="#" class="btn btn-default btn-flat"
+                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Sign
+                                    out</a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                    style="display: none;">
+                                    @csrf
+                                </form>
                             </div>
                         </li>
                     </ul>
