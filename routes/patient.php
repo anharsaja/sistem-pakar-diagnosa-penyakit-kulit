92a9cbb58d5e8c0dfcf3e8557687b919415217ff
Heri Setyawan <herisetyawan233@gmail.com>
2024-12-15 18:01:26 +0700

    feat: diagnose

--- routes/patient.php
+++ routes/patient.php
@@ -6,7 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix'=> '/patient'], function () {
     Route::get('/diagnosa', [DiagnoseController::class, 'index'])->name('diagnosa');
     Route::post('/diagnosa', [DiagnoseController::class, 'diagnosa'])->name('diagnosa');
-    Route::get('/hasil', function () {
-        return view('welcome');
-    })->name('hasil');
+    Route::get('/hasil', [DiagnoseController::class, 'result'])->name('hasil');
 });
